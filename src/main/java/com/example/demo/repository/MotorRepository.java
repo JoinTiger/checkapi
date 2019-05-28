e1461d8398b3aee22141f3bba8beaad02fdf9ce1
luyiwei <1248153626@qq.com>
2019-05-28 13:24:03 +0800

    201905281324

--- src/main/java/com/example/demo/repository/MotorRepository.java
+++ src/main/java/com/example/demo/repository/MotorRepository.java
@@ -11,7 +11,7 @@ public interface MotorRepository extends JpaRepository<Motor, Long> {
     public List<Motor> findByProductId(Long productId);
 
     @Query(value = "select elecName as name, max(cur) as max, min(cur) as min, avg(cur) as average from motor" +
-            " where elecModel = ?1" +
+            " where 1 = 1 and if(isnull(?1) || length(trim(?1)) < 1, 1 = 0, elecModel = ?1)" +
             " group by elecName", nativeQuery = true)
     public List<ElecAtrribueSummary> findAtrributeByElecModel(String elecModel);
 }
